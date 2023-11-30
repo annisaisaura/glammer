98e0b53322b0a12e38a0bc76f4563a2ac9f2bdbf
hasna <aliyah03hasna@gmail.com>
2023-11-30 16:01:24 +0700

    first try add product category page

--- resources/views/product_category_3.blade.php
+++ resources/views/product_category_3.blade.php
@@ -7,6 +7,91 @@
     <title>Product Category</title>
 </head>
 <body>
-    
+    <!--------------- Products Category ------------------->
+</br>
+<div class="small-container">
+  <div class="row row-2">
+      <div class="col-2">
+          <a href="product_category3.html"><img src="dress1.png" ></a>
+          <p>Dresses</p>
+      </div>
+      <div class="col-2">
+          <a href=""><img src="tops1.png" ></a>
+          <p>Tops</p>
+      </div>
+      <div class="col-2">
+          <a href=""><img src="pants1.png" ></a>
+          <p>Pants</p>
+      </div>
+      <div class="col-2">
+          <a href=""><img src="skirt2.png" ></a>
+          <p>Skirt</p>
+      </div>
+      <div class="col-2">
+        <a href=""><img src="beachwear3.png" ></a>
+        <p>Beachwear</p>
+    </div>
+    <div class="col-2">
+      <a href=""><img src="shirts1.png" ></a>
+      <p>Shirts</p>
+    </div>
+  </div>  
+</div>
+</br></br>
+<!--------------- Products ------------------->
+<div class="small-container">
+  <div class="row">
+    <div class="col-4">
+        <a href=""><img src="dress1.png" ></a>
+        <a href=""><h4>Raie Printed Bandeau..</h4></a>
+        <a href=""><p>Rp1.000.000</p></a>
+    </div>
+    <div class="col-4">
+        <a href=""><img src="dress2.png" ></a>
+        <a href=""><h4>Maia Cutout Linen Mini..</h4></a>
+        <a href=""><p>Rp1.000.000</p></a>
+    </div>
+    <div class="col-4">
+        <a href=""><img src="dress4.png" ></a>
+        <a href=""><h4>Astra Floral Linen Midi..</h4></a>
+        <a href=""><p>Rp1.000.000</p></a>
+    </div>
+    <div class="col-4">
+        <a href=""><img src="dress5.png" ></a>
+        <a href=""><h4>Astra Floral Drawstring..</h4></a>
+        <a href=""><p>Rp1.000.000</p></a>
+    </div>
+  </div>
+
+  <div class="row">
+    <div class="col-4">
+        <a href="product_detail.html"><img src="dress6.png" ></a>
+        <a href="product_detail.html"><h4>Gradient Skirt</h4></a>
+        <a href="product_detail.html"><p>Rp1.000.000</p></a>
+    </div>
+    <div class="col-4">
+        <a href=""><img src="dress7.png" ></a>
+        <a href=""><h4>Floral Neck String Top</h4></a>
+        <a href=""><p>Rp1.000.000</p></a>
+    </div>
+    <div class="col-4">
+        <a href=""><img src="dress8.png" ></a>
+        <a href=""><h4>Ginger Scarf Printed..</h4></a>
+        <a href=""><p>Rp1.000.000</p></a>
+    </div>
+    <div class="col-4">
+        <a href=""><img src="dress9.png" ></a>
+        <a href=""><h4>Pink Floral Ditsy Chiffon..</h4></a>
+        <a href=""><p>Rp1.000.000</p></a>
+    </div>
+  </div>
+</div>
+</br></br>
+<div class="center-container">
+  <div class="page-btn">
+    <a href="product_category.html"><span>1</span></a>
+    <a href="product_category2.html"><span>2</span></a>
+  </div>
+</div>
 </body>
 </html>
