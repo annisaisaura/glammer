6a82bb2330a820fda3d5498f43e0afcb95b39bbd
hasna <aliyah03hasna@gmail.com>
2023-11-30 13:19:01 +0700

    starting place product category

--- resources/views/product_category.blade.php
+++ resources/views/product_category.blade.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="{{ asset('css/category.css')}}">
     <title>Product Category</title>
 </head>
 <body>
