06290a6c1f5b24849d0860491c62c8e6ece96a0c
adishazzahra <adishaapaz@gmail.com>
2023-12-01 15:03:00 +0700

    testmain

--- resources/views/detail.blade.php
+++ resources/views/detail.blade.php
@@ -5,13 +5,14 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="{{ asset('css/style.css') }}" />
+    <link rel="stylesheet" href="{{ asset('css/detail.css') }}" />
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap"
         rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
     <title>glammer</title>
 </head>
 
+@include navbar
 <body class="custom-bg-container">
     <div class="container custom-bg-container">
         <div class="container p-5 my-5 ">
