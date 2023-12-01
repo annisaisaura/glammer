d76edab007d42daffbce6f728760aba457d5f5ef
adishazzahra <adishaapaz@gmail.com>
2023-12-01 09:57:56 +0700

    test3

--- resources/views/bismillah.blade.php
+++ resources/views/bismillah.blade.php
@@ -15,6 +15,12 @@
             /* Ganti dengan warna yang diinginkan */
         }
 
+        .custom-bg-container {
+            background-color: #fff5ee;
+            ;
+            /* Ganti dengan warna yang diinginkan */
+        }
+
         .custom-col-size {
             width: 420px;
             /* Atur lebar kolom */
@@ -58,13 +64,35 @@
             border-radius: 10px
         }
 
-        .custom-col-size2{
+        .custom-col-size2 {
             width: 69px;
             /* Atur lebar gambar sesuai kebutuhan */
             height: 57px;
             /* Biarkan tinggi otomatis menyesuaikan agar proporsi gambar tidak terdistorsi */
             border-radius: 10px;
             border-color: #2F4F4F;
+            background-color: #FFF5EE;
+        }
+
+        .custom-col-size3 {
+            width: 250px;
+            /* Atur lebar gambar sesuai kebutuhan */
+            height: 70px;
+            /* Biarkan tinggi otomatis menyesuaikan agar proporsi gambar tidak terdistorsi */
+            border-radius: 10px;
+            border-color: #2F4F4F;
+            background-color: #FFF5EE;
+        }
+
+        .transparent-button {
+            background-color: #FFF5EE;
+            /* Merah dengan tingkat transparansi 0.5 */
+            padding: 10px 20px;
+            /* Sesuaikan padding sesuai kebutuhan Anda */
+        }
+
+        .custom-col-size-opt1 {
+            width: 460px;
         }
 
         .custom-text {
@@ -74,12 +102,26 @@
     </style>
 </head>
 
-<body>
-    <div class="container">
-        <div class="container p-5 my-5 border">
-            <div class="row m-0 p-0 ">
-                <div class="col-sm-5 text-center  custom-bg-lightgray custom-col-size">
-                    <img src="{{ asset('img/dress6.png') }}" alt="dress6" class="custom-img">
+<body class="custom-bg-container">
+    <div class="container custom-bg-container">
+        <div class="container p-5 my-5 ">
+            <div class="row m-0 p-0 gap-3">
+                <div class="row custom-col-size gap-2">
+                    <div class="col-sm-4 text-center custom-bg-lightgray custom-col-size p-2">
+                        <img src="{{ asset('img/dress6.png') }}" alt="dress6" class="custom-img">
+                    </div>
+                    <div class="col-sm-2 text-center  custom-bg-lightgray custom-col-size-opt mt-2 p-2">
+                        <img src="{{ asset('img/dress6.png') }}" alt="dress6" class="custom-img-opt">
+                    </div>
+                    <div class="col-sm-2 text-center  custom-bg-lightgray custom-col-size-opt mt-2 p-2">
+                        <img src="{{ asset('img/dress6.png') }}" alt="dress6" class="custom-img-opt">
+                    </div>
+                    <div class="col-sm-2 text-center  custom-bg-lightgray custom-col-size-opt mt-2 p-2">
+                        <img src="{{ asset('img/dress6.png') }}" alt="dress6" class="custom-img-opt">
+                    </div>
+                    <div class="col-sm-2 text-center  custom-bg-lightgray custom-col-size-opt mt-2 p-2">
+                        <img src="{{ asset('img/dress6.png') }}" alt="dress6" class="custom-img-opt">
+                    </div>
                 </div>
                 <div class="col-sm-7">
                     <div class="row m-4 ml-2 mr-2 mt-0 mb-2 fs-3 fw-normal custom-text">
@@ -112,53 +154,47 @@
                         Size
                     </div>
                     <div class="row m-4 ml-2 mr-2 mt-2 mb-2 gap-2">
-                        <div class="col-sm-2 text-center border custom-col-size2 p-3 custom-text">
-                           XL
-                        </div>
-                        <div class="col-sm-2 text-center border custom-col-size2 p-3 custom-text">
+                        <button class="col-sm-2 text-center border transparent-button custom-col-size2 p-3 custom-text">
+                            XL
+                        </button>
+                        <button class="col-sm-2 text-center border transparent-button custom-col-size2 p-3 custom-text">
                             L
-                        </div>
-                        <div class="col-sm-2 text-center border custom-col-size2 p-3 custom-text">
+                        </button>
+                        <button class="col-sm-2 text-center border transparent-button custom-col-size2 p-3 custom-text">
                             M
-                        </div>
-                        <div class="col-sm-2 text-center border custom-col-size2 p-3 custom-text">
+                        </button>
+                        <button class="col-sm-2 text-center border transparent-button custom-col-size2 p-3 custom-text">
                             S
-                        </div>
+                        </button>
                     </div>
                     <div class="row m-4 ml-2 mr-2 mt-4 mb-2 fs-6 fw-normal custom-text">
                         Quantity
                     </div>
                     <div class="row m-4 ml-2 mr-2 mt-2 mb-2">
-                        <div class="col-sm-2 text-center border custom-col-size2 p-3 custom-text">
-                           +
-                        </div>
+                        <button class="col-sm-2 text-center border transparent-button custom-col-size2 p-3 custom-text">
+                            +
+                        </button>
                         <div class="col-sm-2 text-center border custom-col-size2 p-3 custom-text">
                             1
                         </div>
-                        <div class="col-sm-2 text-center border custom-col-size2 p-3 custom-text">
+                        <button class="col-sm-2 text-center border transparent-button custom-col-size2 p-3 custom-text">
                             -
-                        </div>
+                        </button>
+                    </div>
+                    <div class="row m-4 ml-2 mr-2 mt-4 mb-2 gap-2"></div>
+                    <div class="row m-4 ml-2 mr-2 mt-4 mb-2 gap-2">
+                        <button class="col-sm-2 text-center border transparent-button custom-col-size3 p-3 custom-text">
+                            <img src="{{ asset('img/dress6.png') }}" alt="dress6" class="custom-img">
+                            Add To Cart
+                        </button>
+                        <button class="col-sm-2 text-center border transparent-button custom-col-size3 p-3 custom-text">
+                            Buy Now
+                        </button>
                     </div>
                 </div>
             </div>
-
-            <div class="row m-0 p-0 gap-3">
-                <div class="col-sm-2 text-center  custom-bg-lightgray custom-col-size-opt mt-2 p-2">
-                    <img src="{{ asset('img/dress6.png') }}" alt="dress6" class="custom-img-opt">
-                </div>
-                <div class="col-sm-2 text-center  custom-bg-lightgray custom-col-size-opt mt-2 p-2">
-                    <img src="{{ asset('img/dress6.png') }}" alt="dress6" class="custom-img-opt">
-                </div>
-                <div class="col-sm-2 text-center  custom-bg-lightgray custom-col-size-opt mt-2 p-2">
-                    <img src="{{ asset('img/dress6.png') }}" alt="dress6" class="custom-img-opt">
-                </div>
-                <div class="col-sm-2 text-center  custom-bg-lightgray custom-col-size-opt mt-2 p-2">
-                    <img src="{{ asset('img/dress6.png') }}" alt="dress6" class="custom-img-opt">
-                </div>
-            </div>
-
         </div>
-
+    </div>
     </div>
 </body>
 
--- resources/views/productDetails.blade.php
+++ resources/views/productDetails.blade.php
@@ -3,7 +3,7 @@
 
 <head>
     <meta charset="utf-8" />
-    <meta name="viewport" content="initial-scale=1, width=device-width" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
 
     <link rel="stylesheet" href="{{ asset('css/productDetails.css') }}" />
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Poppins:wght@400&display=swap" />
